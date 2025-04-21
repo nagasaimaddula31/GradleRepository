6e2909e6ef4e1a0b1dbfc0baa54bd6b1c73b3f70
nagasaimaddula <nagasaimaddula315@gmail.com>
2025-04-21 19:37:17 +0530

    gradleCommit

---
+++ src/test/java/starter/CucumberTestSuite.java
@@ -0,0 +1,18 @@
+package starter;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "starter.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,io.cucumber.core.plugin.SerenityReporter")
+
+public class CucumberTestSuite {
+}
